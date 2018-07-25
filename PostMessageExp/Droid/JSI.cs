90c5a27115f9c1ab134f50173030041e6b9ba536
vincenzo aceto <vincenzo.aceto-apple@unina.it>
2018-07-25 10:21:12 +0200

    Added javascript interface in android

---
+++ PostMessageExp/Droid/JSI.cs
@@ -0,0 +1,22 @@
+﻿using Android.Webkit;
+using Java.Interop;
+
+namespace PostMessageExp.Droid
+{
+    public class JSI : Java.Lang.Object
+    {
+
+        public JSI()
+        {
+            
+        }
+
+        [Export]
+        [JavascriptInterface]
+        public void NativeFunction()
+        {
+            System.Diagnostics.Debug.WriteLine("Native Function Called");
+        }
+
+    }
+}
