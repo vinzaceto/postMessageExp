d0fd865cf5c0c3cdfd15f0bd450511e6f2bdc96c
Mazza_Andrea <Andrea11.Mazza@nttdata.com>
2018-07-31 11:02:07 +0200

    [PCL] Update WeView Manager

--- PostMessageExp/PostMessageExp/Managers/CustomWebviewManager.cs
+++ PostMessageExp/PostMessageExp/Managers/CustomWebviewManager.cs
@@ -1,10 +1,54 @@
 ﻿using System;
+using System.Threading.Tasks;
+
 namespace PostMessageExp
 {
-    public class CommunicationManager
+    public class CustomWebviewManager
     {
-        public CommunicationManager()
+        private static object lockerInterface = new object();
+        private static CustomWebviewInterface interfacesf;
+
+
+        /// <summary>
+        /// Metodo di utilità che invocando il servizio BE rotprnerà i parametri di configurazione webview 
+        /// tra i quali anche l'url da passare alla pagina
+        /// </summary>
+        /// <returns>The configurations.</returns>
+        /// <param name="chiaveServizio">Chiave servizio.</param>
+        /// <param name="codiceFornitura">Codice fornitura.</param>
+        static public async Task GetConfigurations(string chiaveServizio, string codiceFornitura)
         {
+            try
+            {
+               //TODO
+                return null;
+            }
+            catch (Exception ex)
+            {
+               //TODO ADD LOGMANAGER
+            }           
         }
+
+
+        #region PROPERTIES
+        public CustomWebviewInterface GetIntInstance()
+        {
+            lock (lockerInterface)
+            {
+                return interfacesf;
+            }
+        }
+
+        public void SetIntInstance(CustomWebviewInterface interfacesf)
+        {
+            lock (lockerInterface)
+            {
+                CustomWebviewManager.interfacesf = interfacesf;
+            }
+        }
+        #endregion
     }
+
+
+  
 }
