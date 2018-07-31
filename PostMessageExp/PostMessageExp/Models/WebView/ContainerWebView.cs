bc433477d2182344ba35d659dc06f573de562ab7
Mazza_Andrea <Andrea11.Mazza@nttdata.com>
2018-07-31 15:44:43 +0200

    [PCL] Add ContainerWebView

---
+++ PostMessageExp/PostMessageExp/Models/WebView/ContainerWebView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace PostMessageExp.Models.WebView
+{
+  
+    public class ContainerWebView
+    {
+        public string idProcesso { get; set; }
+        public string idUtente { get; set; }      
+        public string tipoOp { get; set; }
+     
+        #region TODO Aggiungere eventuali campi in base alle tipologie di webbview
+      
+        #endregion
+       
+        public ContainerWebView()
+        {
+            
+            //TODO Aggiungere alla sessione corrente IdProcesso 
+            //SessionManager.Instance.IdProcesso = Guid.NewGuid();
+            this.idProcesso = Guid.NewGuid().ToString();
+        }
+
+       
+    }
+
+}
